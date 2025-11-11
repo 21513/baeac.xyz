e3c443f02e5c5babf215a7f0788fc499b349fa25
baeac <baeacbusiness@gmail.com>
2025-11-11 23:31:08 +0100

    Refactor navigation button to scroll to top; update styles and improve functionality

--- main.js
+++ main.js
@@ -1,35 +1,26 @@
 import { setupThreeJS } from './modules/index.three.js';
-import { mouseLocation, scrollAmount, scrollLocation, mouseHorizontal, mouseVertical } from './modules/mouse.js';
+import { mouseLocation, scrollLocation, socialsFollowScroll } from './modules/mouse.js';
 import { scrollToTop } from './modules/navigation.js';
 
 document.addEventListener('DOMContentLoaded', setupThreeJS);
 
 document.addEventListener('mousemove', mouseLocation);
-document.addEventListener('scroll', scrollLocation);
 
-const logo = document.getElementById('logo_button');
+document.addEventListener('scroll', scrollLocation);
+document.addEventListener('scroll', socialsFollowScroll);
+
+const logo = document.getElementById('scroll_button');
 logo.addEventListener('click', scrollToTop);
 
 document.addEventListener('scroll', () => {
-    const socialsCard = document.getElementById('socials_card');
+    const scrollToTopButton = document.getElementById('scroll_button');
+    const windowHeight = window.innerHeight;
 
-    const navigationHeight = document.getElementById('top_navigation').clientHeight;
-    const canvasHeight = document.getElementById('three_js').clientHeight;
-    const totalHeight = document.body.clientHeight;
-    const socialsCardHeight = socialsCard.clientHeight;
-
-    const socialsScrollHeight = canvasHeight + navigationHeight - ((totalHeight / 2) - (socialsCardHeight / 2));
-
-    let scrollScale = 1.1 - ((scrollAmount / socialsScrollHeight) * 0.1);
-
-    if (scrollAmount < socialsScrollHeight) {
-        socialsCard.style.position = 'fixed';
-        socialsCard.style.top = '50%';
-        socialsCard.style.transform = 'translateY(-50%)';
-        socialsCard.style.scale = scrollScale;
+    if (window.scrollY > (windowHeight / 3)) {
+        scrollToTopButton.style.opacity = '1';
+        scrollToTopButton.style.pointerEvents = 'auto';
     } else {
-        socialsCard.style.position = 'inherit';
-        socialsCard.style.top = 'initial';
-        socialsCard.style.transform = 'none';
+        scrollToTopButton.style.opacity = '0';
+        scrollToTopButton.style.pointerEvents = 'none';
     }
 });
--- modules/mouse.js
+++ modules/mouse.js
@@ -20,3 +20,27 @@ export function scrollLocation(event) {
 
     scrollAmount = window.scrollY;
 }
+
+export function socialsFollowScroll(event) {
+    const socialsCard = document.getElementById('socials_card');
+
+    const navigationHeight = document.getElementById('top_navigation').clientHeight;
+    const canvasHeight = document.getElementById('three_js').clientHeight;
+    const totalHeight = document.body.clientHeight;
+    const socialsCardHeight = socialsCard.clientHeight;
+
+    const socialsScrollHeight = canvasHeight + navigationHeight - ((totalHeight / 2) - (socialsCardHeight / 2));
+
+    let scrollScale = 1.1 - ((scrollAmount / socialsScrollHeight) * 0.1);
+
+    if (scrollAmount < socialsScrollHeight) {
+        socialsCard.style.position = 'fixed';
+        socialsCard.style.top = '50%';
+        socialsCard.style.transform = 'translateY(-50%)';
+        socialsCard.style.scale = scrollScale;
+    } else {
+        socialsCard.style.position = 'inherit';
+        socialsCard.style.top = 'initial';
+        socialsCard.style.transform = 'none';
+    }
+}

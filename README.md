54d33f612c4e86f5944059abfc5d1c0e93c4d5f5
baeac <baeacbusiness@gmail.com>
2025-11-11 22:21:11 +0100

    Refactor navigation and social card behavior; remove unused settings and update README

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # baeac.xyz
-
-This website is made to offer free knowledge and resources in the 3D industry.
-
-There is currently no mobile support, please visit on a desktop or laptop.
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { setupThreeJS } from './modules/index.three.js';
-import { mouseLocation, scrollLocation } from './modules/mouse.js';
+import { mouseLocation, scrollAmount, scrollLocation, mouseHorizontal, mouseVertical } from './modules/mouse.js';
 import { scrollToTop } from './modules/navigation.js';
 
 document.addEventListener('DOMContentLoaded', setupThreeJS);
@@ -9,3 +9,27 @@ document.addEventListener('scroll', scrollLocation);
 
 const logo = document.getElementById('logo_button');
 logo.addEventListener('click', scrollToTop);
+
+document.addEventListener('scroll', () => {
+    const socialsCard = document.getElementById('socials_card');
+
+    const navigationHeight = document.getElementById('top_navigation').clientHeight;
+    const canvasHeight = document.getElementById('three_js').clientHeight;
+    const totalHeight = document.body.clientHeight;
+    const socialsCardHeight = socialsCard.clientHeight;
+
+    const socialsScrollHeight = canvasHeight + navigationHeight - ((totalHeight / 2) - (socialsCardHeight / 2));
+
+    let scrollScale = 1.1 - ((scrollAmount / socialsScrollHeight) * 0.1);
+
+    if (scrollAmount < socialsScrollHeight) {
+        socialsCard.style.position = 'fixed';
+        socialsCard.style.top = '50%';
+        socialsCard.style.transform = 'translateY(-50%)';
+        socialsCard.style.scale = scrollScale;
+    } else {
+        socialsCard.style.position = 'inherit';
+        socialsCard.style.top = 'initial';
+        socialsCard.style.transform = 'none';
+    }
+});

62d549336b7a3e8f42536a85e2c6550bb7a31f79
baeac <baeacbusiness@gmail.com>
2025-11-12 19:29:30 +0100

    Refactor product card structure and update navigation links for consistency

--- modules/navigation.js
+++ modules/navigation.js
@@ -6,11 +6,22 @@ export function toggleScrollButton() {
     const scrollToTopButton = document.getElementById('scroll_button');
     const windowHeight = window.innerHeight;
 
-    if (window.scrollY > (windowHeight / 3)) {
-        scrollToTopButton.style.opacity = '1';
-        scrollToTopButton.style.pointerEvents = 'auto';
-    } else {
+    const totalHeight = document.documentElement.scrollHeight;
+    const footerHeight = document.getElementById('footer').clientHeight - 8;
+
+    if (window.scrollY < (windowHeight / 3)) {
         scrollToTopButton.style.opacity = '0';
         scrollToTopButton.style.pointerEvents = 'none';
+        scrollToTopButton.style.transform = 'none';
+    } else if(window.scrollY >= (windowHeight / 3) && window.scrollY < (totalHeight - windowHeight - 100)) {
+        scrollToTopButton.style.opacity = '1';
+        scrollToTopButton.style.pointerEvents = 'auto';
+        scrollToTopButton.style.transform = 'none';
+    } else {
+        scrollToTopButton.style.opacity = '1';
+        scrollToTopButton.style.pointerEvents = 'auto';
+        if (document.body.clientWidth < 1024) {
+            scrollToTopButton.style.transform = `translateY(-${footerHeight}px)`;
+        }
     }
 }
